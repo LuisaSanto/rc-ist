9b4c49fa9ae266f7161bc145deeab19d20bb1b0f
LuisaSanto <luisa.santo@gmail.com>
2018-10-05 23:01:35 +0100

    Add LFD

--- backupserver.py
+++ backupserver.py
@@ -1,9 +1,13 @@
 import socket
 import sys
 import argparse
-import multiprocessing as Process
+import os as os
+from os import listdir
+from os.path import isfile, join
+import time
+from datetime import datetime
 
-def servingCS(socketUDP, portUDP):
+def servingCS(socketUDP, portUDP, users):
     print("Awaiting contact from CS")
     try:
         socketUDP.settimeout(300.0)
@@ -15,14 +19,51 @@ def servingCS(socketUDP, portUDP):
     print("Message received from {]".format(address[0], str(address[1])))
     print("Content: {}".format(message))
 
-    if reply[:3] == "LSF":
-        print("LSF")
+    if message[:3] == "LSF":
+        if len(reply) != 3:
+            print("ERR! Message sent from server is corrupted")
+            try:
+                serverUDP.sendto("LFD ERR\n")
+                return
+            except socket.error as err:
+                print("Error sending message to client")
+        user_name = reply[1]
+        user_dir = reply[2]
+        print("Getting user's name directory")
+        if users_dict.get(user_name):
+            path = user_name + "/" + user_dir
+            onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+            time = []
+            size = []
+            for file in onlyfiles:
+                files_path = path + "/" + file
+                size += [os.path.getsize(files_path)]
+                time += [time.ctime(os.path.getmtime(files_path))]  # TODO: DATE FORMAT
+            print("Sending LSF reply message")
+            try:
+                merge_list = list(zip(onlyfiles, time, size))
+                serverUDP.sendto("LFD" + len(onlyfiles) + merge_list)
+            except socket.error as err:
+                print("Error sending message to Central Server: {}".format(err))
+                return
+            
+    elif message[:3] == "LSU":
+        if len(reply) != 3:
+            print("ERR! Message sent from server is corrupted")
+            try:
+                serverUDP.sendto("LUR ERR\n")
+                return
+            except socket.error as err:
+                print("Error sending message to client")
 
-    elif reply[:3] == "LSU":
-        print("LSU")
-
-    elif reply[:3] == "DLB":
-        print("DLB")
+    elif message[:3] == "DLB":
+        if len(reply) != 3:
+            print("ERR! Message sent from server is corrupted")
+            try:
+                serverUDP.sendto("DBR ERR\n")
+                return
+            except socket.error as err:
+                print("Error sending message to client")
 
     else:
         print("Message received with wrong format")
@@ -96,13 +137,24 @@ try:
 
         print("Registation completed with success")
 
-        servingCS(serverUDP, portBS)
 
     except:
         print("Backup Server not responding. Try again.")
         sys.exit("Shutting down Backup Server.")
 
     isRegisted = 1
+
+    users_file = open("users.txt", "r")
+    user_lines = users_file.readlines()
+    users = []
+    pw = []
+    for i in range(len(user_lines)):
+        pair = user_lines[i].split(" ")
+        users += [pair[0]]
+        pw += [pair[1].rstrip()]
+    users_dict = dict(zip(users, pw))
+    servingCS(serverUDP, portBS, users_dict)
+
 
     # TODO: KEEP HERE
 
