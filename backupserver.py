b2f8aa5db6586cc62f6db02c1cabe130f84d4e59
LuisaSanto <luisa.santo@gmail.com>
2018-10-05 10:23:39 +0100

    Add parse args on terminal

--- backupserver.py
+++ backupserver.py
@@ -1,7 +1,35 @@
 import socket
 import sys
-import os
-import math
-import errno
 import argparse
 
+try:
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-b", help="Backup Server port", type=int)
+    parser.add_argument("-n", help="Central Server host")
+    parser.add_argument("-p", help="Central Server port", type=int)
+
+    try:
+        arguments = parser.parse_args()
+    except:
+        print("Error in format")
+        print("Try:-b 59000 -n tejo.ist.utl.pt -p 58001")
+        sys.exit(0)
+
+    # Default values
+    hostCS = socket.gethostbyname()
+    portCS = 59001
+    portBS = 59000
+
+    # parse inserted args
+    if arguments.b: portBS = arguments.b
+    if arguments.n: hostCS = arguments.n
+    if arguments.p: portCS = arguments.p
+
+except KeyboardInterrupt:
+    print('\n KeyboardInterrupt found')
+
+finally:
+    # TODO: VER DISTO
+    sys.exit(0)
