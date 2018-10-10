d49f2ad4c509a9a144e6ecb811196180b6561550
LuisaSanto <luisa.santo@gmail.com>
2018-10-10 18:17:54 +0100

    final version bacjupserver

--- backupserver.py
+++ backupserver.py
@@ -90,127 +90,126 @@ def update_users():
 
 
 def servingCS(socketUDP):
-    while 1:
-        print("Awaiting contact from CS")
-        try:
-            socketUDP.settimeout(300.0)
-            message, address = socketUDP.recvfrom(1024)
-        except Exception:
-            print("5 minutes have passed. Waiting 5 more minutes.")
-            return
-        reply = message.split(' ')
-        print("Message received from {]".format(address[0], str(address[1])))
-        print("Content: {}".format(message))
+    print("Awaiting contact from CS")
+    try:
+        socketUDP.settimeout(300.0)
+        message, address = socketUDP.recvfrom(1024)
+    except Exception:
+        print("5 minutes have passed. Waiting 5 more minutes.")
+        return
+    reply = message.split(' ')
+    print("Message received from {]".format(address[0], str(address[1])))
+    print("Content: {}".format(message))
 
-        if message[:3] == "LSF":
-            if len(reply) != 3:
-                print("ERR! Message sent from server is corrupted")
-                try:
-                    serverUDP.sendto("LFD ERR\n")
-                except socket.error as err:
-                    print("Error sending message to client")
-                    return
-            user_name = reply[1]
-            user_dir = reply[2]
-            print("Getting user's name directory")
-            users_dict = update_users()
-            if users_dict.get(user_name):
-                path = user_name + "/" + user_dir
-                onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
-                time = []
-                size = []
-                for file in onlyfiles:
-                    files_path = path + "/" + file
-                    size += [os.path.getsize(files_path)]
-                    time += [time.ctime(os.path.getmtime(files_path))]  # TODO: DATE FORMAT
-                print("Sending LSF reply message")
-                try:
-                    merge_list = list(zip(onlyfiles, time, size))
-                    serverUDP.sendto("LFD" + len(onlyfiles) + merge_list)
-                except socket.error as err:
-                    print("LFD ERR\n")
-                    print("Error sending message to Central Server: {}".format(err))
-                    serverUDP.sendto("LFD ERR\n")
-                    return
-            else:
-                print("User not found.")
+    if message[:3] == "LSF":
+        if len(reply) != 3:
+            print("ERR! Message sent from server is corrupted")
+            try:
                 serverUDP.sendto("LFD ERR\n")
-
-        elif message[:3] == "LSU":
-            if len(reply) != 3:
-                print("ERR! Message sent from server is corrupted")
-                try:
-                    serverUDP.sendto("LUR ERR\n")
-                except socket.error as err:
-                    print("Error sending message to client")
-                    return
-            user_name = reply[1]
-            user_pw = reply[2]
-            user = user_name + " " + user_pw
-            print("Adding new user to user's list")
-            users_file = open("users.txt", "a")
-            users_file.write(user)
-            users_file.close()
-            try:
-                serverUDP.sendto("LUR OK\n")
             except socket.error as err:
-                print("LUR ERR\n")
-                print("Error sending message to Central Server: {}".format(err))
-                serverUDP.sendto("LUR ERR\n")
+                print("Error sending message to client")
                 return
-
-        elif message[:3] == "DLB":
-            if len(reply) != 3:
-                print("ERR! Message sent from server is corrupted")
-                try:
-                    serverUDP.sendto("DBR ERR\n")
-                except socket.error as err:
-                    print("Error sending message to client")
-                    return
-            user_name = reply[1]
-            user_dir = reply[2]
-            user = user_name + " " + user_dir
-
-            print("Removing user's directory")
+        user_name = reply[1]
+        user_dir = reply[2]
+        print("Getting user's name directory")
+        users_dict = update_users()
+        if users_dict.get(user_name):
+            path = user_name + "/" + user_dir
+            onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+            time = []
+            size = []
+            for file in onlyfiles:
+                files_path = path + "/" + file
+                size += [os.path.getsize(files_path)]
+                time += [time.ctime(os.path.getmtime(files_path))]  # TODO: DATE FORMAT
+            print("Sending LSF reply message")
             try:
-                shutil.rmtree(user)
-            except Exception as err:
-                print("Error in removing user directory")
-                socketUDP.sendto("DBR NOK\n")
-                return
-
-            print("Removing user in user's list")
-            try:
-                users_file = open("users.txt", "r")
-                lines = users_file.readlines()
-                users_file.close()
-                for i in range(len(lines)):
-                    if user_name in lines[i]:
-                        del lines[i]
-                        break
-                users_file = open("users.txt", "w")
-                for line in lines:
-                    users_file.write(line)
-                users_file.close()
-                update_users()
-            except IOError as err:
-                print("Error trying to remove user from BS")
-                serverUDP.sendto("DBR ERR\n")
-                return
-
-            print("Removal completed")
-            update_users()
-            try:
-                serverUDP.sendto("DLB OK\n")
+                merge_list = list(zip(onlyfiles, time, size))
+                serverUDP.sendto("LFD" + len(onlyfiles) + merge_list)
             except socket.error as err:
-                print("DBR ERR\n")
+                print("LFD ERR\n")
                 print("Error sending message to Central Server: {}".format(err))
-                serverUDP.sendto("DBR ERR\n")
+                serverUDP.sendto("LFD ERR\n")
                 return
-
         else:
-            print("Message received with wrong format")
+            print("User not found.")
+            serverUDP.sendto("LFD ERR\n")
+
+    elif message[:3] == "LSU":
+        if len(reply) != 3:
+            print("ERR! Message sent from server is corrupted")
+            try:
+                serverUDP.sendto("LUR ERR\n")
+            except socket.error as err:
+                print("Error sending message to client")
+                return
+        user_name = reply[1]
+        user_pw = reply[2]
+        user = user_name + " " + user_pw
+        print("Adding new user to user's list")
+        users_file = open("users.txt", "a")
+        users_file.write(user)
+        users_file.close()
+        try:
+            serverUDP.sendto("LUR OK\n")
+        except socket.error as err:
+            print("LUR ERR\n")
+            print("Error sending message to Central Server: {}".format(err))
+            serverUDP.sendto("LUR ERR\n")
             return
+
+    elif message[:3] == "DLB":
+        if len(reply) != 3:
+            print("ERR! Message sent from server is corrupted")
+            try:
+                serverUDP.sendto("DBR ERR\n")
+            except socket.error as err:
+                print("Error sending message to client")
+                return
+        user_name = reply[1]
+        user_dir = reply[2]
+        user = user_name + " " + user_dir
+
+        print("Removing user's directory")
+        try:
+            shutil.rmtree(user)
+        except Exception as err:
+            print("Error in removing user directory")
+            socketUDP.sendto("DBR NOK\n")
+            return
+
+        print("Removing user in user's list")
+        try:
+            users_file = open("users.txt", "r")
+            lines = users_file.readlines()
+            users_file.close()
+            for i in range(len(lines)):
+                if user_name in lines[i]:
+                    del lines[i]
+                    break
+            users_file = open("users.txt", "w")
+            for line in lines:
+                users_file.write(line)
+            users_file.close()
+            update_users()
+        except IOError as err:
+            print("Error trying to remove user from BS")
+            serverUDP.sendto("DBR ERR\n")
+            return
+
+        print("Removal completed")
+        update_users()
+        try:
+            serverUDP.sendto("DLB OK\n")
+        except socket.error as err:
+            print("DBR ERR\n")
+            print("Error sending message to Central Server: {}".format(err))
+            serverUDP.sendto("DBR ERR\n")
+            return
+
+    else:
+        print("Message received with wrong format")
+        return
 
 
 def servingUser(serverTCP):
